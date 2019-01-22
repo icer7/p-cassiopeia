707353962f0de7027a37885d3b244d38b2577e38
ryo <ryo0104777@gmail.com>
2019-01-22 13:19:39 +0900

    側面撮影完成

--- app/controllers/measurement_result_controller.rb
+++ app/controllers/measurement_result_controller.rb
@@ -2,7 +2,6 @@ class MeasurementResultController < ApplicationController
 
   def home
     @value = params[:value]
-    binding.pry
     render :file => "/app/views/measurement_result.html.erb"
   end
 end
